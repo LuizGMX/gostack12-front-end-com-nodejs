8df8db79d0a07c87b261cf09e813aa1f8d588bc1
Luiz Gustavo <luizguestape54@hotmail.com>
2020-07-01 15:39:42 -0300

    erro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import Header from './components/Header'
+import './App.css';
+import api from './services/api';
 
-function App(){
-    return <h1>Hello World</h1>
-}
+function App() {
+    const [projects,setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    useEffect((()=>{api.get('projects').then(response=>{console.log(response)})},[]))
+
+    function handleAddProject() {
+       // projects.push(`Novo projeto ${Date.now()}`);
+        setProjects([...projects,`Novo projeto ${Date.now()}`]);        
+    }
+
+    return (
+        <>
+            <Header title="Projects" />            
+            <ul>
+                {projects.map(project =>
+                    <li key={project}>{project}</li>
+                )}
+            </ul>
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+        </>
+    );
+};
 
 export default App;
